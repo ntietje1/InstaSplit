649b6b98a2c17b7ad6c4a951f50df2e615f62cbe
Nicholas Tietje <tietje.n@northeastern.edu>
2024-04-04 11:48:11 -0400

    remove unused imports

--- app/src/main/kotlin/com/hypeapps/instasplit/ui/login/LoginComponents.kt
+++ app/src/main/kotlin/com/hypeapps/instasplit/ui/login/LoginComponents.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -15,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
