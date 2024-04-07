b9adc01aa563a2872099a83704253b04b6827f7b
Nicholas Tietje <tietje.n@northeastern.edu>
2024-04-07 11:10:11 -0400

    add processing state

--- app/src/main/kotlin/com/hypeapps/instasplit/ui/camera/photo_capture/CameraState.kt
+++ app/src/main/kotlin/com/hypeapps/instasplit/ui/camera/photo_capture/CameraState.kt
@@ -6,4 +6,5 @@ import com.google.mlkit.vision.text.Text
 data class CameraState(
     val capturedImage: Bitmap? = null,
     val extractedElements: Pair<Text.Element, Text.Element>? = null,
+    val isProcessing: Boolean = false
 )
