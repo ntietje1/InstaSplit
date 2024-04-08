2301f8b0c258cdf12ee0a008515e6c21094a56de
Nicholas Tietje <tietje.n@northeastern.edu>
2024-04-08 14:23:07 -0400

    add repository initialization

--- app/src/main/kotlin/com/hypeapps/instasplit/core/db/InstaSplitRepository.kt
+++ app/src/main/kotlin/com/hypeapps/instasplit/core/db/InstaSplitRepository.kt
@@ -1,17 +1,30 @@
 package com.hypeapps.instasplit.core.db
 
-import com.hypeapps.instasplit.core.db.dao.ExpenseDao
-import com.hypeapps.instasplit.core.db.dao.GroupDao
-import com.hypeapps.instasplit.core.db.dao.GroupMemberDao
-import com.hypeapps.instasplit.core.db.dao.UserDao
-import com.hypeapps.instasplit.core.db.dao.UserExpenseDao
+import com.hypeapps.instasplit.core.model.entity.Expense
+import com.hypeapps.instasplit.core.network.InstaSplitApi
 
 class InstaSplitRepository(
-    private val userDao: UserDao,
-    private val groupDao: GroupDao,
-    private val expenseDao: ExpenseDao,
-    private val GroupMemberDao: GroupMemberDao,
-    private val UserExpenseDao: UserExpenseDao
-) {
-    // implement methods to interact with the database here (also with network if we implement that
+    private val database: InstaSplitDatabase,
+    private val api: InstaSplitApi
+    ) {
+    private val expenseDao = database.expenseDao()
+    private val userExpenseDao = database.userExpenseDao()
+    private val groupDao = database.groupDao()
+    private val groupMemberDao = database.groupMemberDao()
+    private val userDao = database.userDao()
+
+    // implement methods to interact with the database and network here
+    // on loading a page, call API to get data, then save to database
+    // on updating/deleting data, call (fake) API to update data, then also make change to database
+    // UI observes livedata from database
+
+    suspend fun getExpenseById(expenseId: Int): Expense {
+        return try {
+            val expense = api.getExpenseById(expenseId)
+            expenseDao.addExpense(expense)
+            expense
+        } catch (e: Exception) {
+            expenseDao.getExpenseById(expenseId)
+        }
+    }
 }
