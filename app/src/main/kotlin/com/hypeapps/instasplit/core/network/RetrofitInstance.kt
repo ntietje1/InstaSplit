ab015ba3176c2f64154427efc523290aeb522011
Nicholas Tietje <tietje.n@northeastern.edu>
2024-04-11 12:17:35 -0400

    add placeholder url

--- app/src/main/kotlin/com/hypeapps/instasplit/core/network/RetrofitInstance.kt
+++ app/src/main/kotlin/com/hypeapps/instasplit/core/network/RetrofitInstance.kt
@@ -10,7 +10,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL: String = "TODO"
+    private const val BASE_URL: String = "https://kgtttq6tg9.execute-api.us-east-2.amazonaws.com/"
 
     val instaSplitApi: InstaSplitApi by lazy {
         Retrofit.Builder()
