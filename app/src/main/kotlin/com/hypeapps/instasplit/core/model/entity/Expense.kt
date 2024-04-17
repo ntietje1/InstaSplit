0b91d134dfd266ee436cb5ae7a53efe15aa5adba
Nicholas Tietje <tietje.n@northeastern.edu>
2024-04-17 12:10:56 -0400

    add formatted date helper method

--- app/src/main/kotlin/com/hypeapps/instasplit/core/model/entity/Expense.kt
+++ app/src/main/kotlin/com/hypeapps/instasplit/core/model/entity/Expense.kt
@@ -4,6 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Entity(
     foreignKeys = [
@@ -21,4 +24,13 @@ data class Expense(
     @ColumnInfo val description: String = "",
     @ColumnInfo val date: Long = 0L,
     @ColumnInfo val totalAmount: Double = 0.00,
-)
+) {
+    val formattedDate: String
+        get() = convertLongToTime(date)
+
+    private fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        return format.format(date)
+    }
+}
