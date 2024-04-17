c8178ff7a918bcefb9a3cbc75f4fba149a51b024
Nicholas Tietje <tietje.n@northeastern.edu>
2024-04-17 12:43:01 -0400

    add getBalanceToUser method

--- app/src/main/kotlin/com/hypeapps/instasplit/core/model/entity/bridge/ExpenseWrapper.kt
+++ app/src/main/kotlin/com/hypeapps/instasplit/core/model/entity/bridge/ExpenseWrapper.kt
@@ -22,7 +22,29 @@ data class ExpenseWrapper(
     )
     val userExpenses: List<UserExpense>
 ) {
+    val evenlySplitAmount: Double
+        get() = expense.totalAmount / users.size
+
+    val paidBy: Int?
+        get() = userExpenses.firstOrNull { it.balance == expense.totalAmount - evenlySplitAmount}?.userId
+
+    fun getBalanceToUser(currentUser: Int, otherUser: Int): Double {
+        val currentUserBalance = getBalance(currentUser)
+        val otherUserBalance = getBalance(otherUser)
+        if (currentUser == paidBy) {
+            return -otherUserBalance
+            // other user OWES money to current user
+        } else if (otherUser == paidBy) {
+            return currentUserBalance
+            // current user OWES money to other user
+        } else {
+            // current user and other user don't owe eachother anything here
+            return 0.0
+        }
+    }
+
     fun getBalance(userId: Int): Double {
         return userExpenses.firstOrNull { it.userId == userId }?.balance ?: 0.0
     }
+
 }
