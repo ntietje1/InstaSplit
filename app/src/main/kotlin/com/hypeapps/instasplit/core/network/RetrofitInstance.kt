d064806b5175927ac8a1748b412b9725975dae2b
Nicholas Tietje <tietje.n@northeastern.edu>
2024-04-17 18:06:25 -0400

    add endpoints for loading images

--- app/src/main/kotlin/com/hypeapps/instasplit/core/network/RetrofitInstance.kt
+++ app/src/main/kotlin/com/hypeapps/instasplit/core/network/RetrofitInstance.kt
@@ -1,6 +1,7 @@
 package com.hypeapps.instasplit.core.network
 
 
+import com.hypeapps.instasplit.core.network.InstaSplitApi.Companion.BASE_URL
 import com.hypeapps.instasplit.core.network.api.ExpenseApi
 import com.hypeapps.instasplit.core.network.api.GroupApi
 import com.hypeapps.instasplit.core.network.api.GroupMemberApi
@@ -8,10 +9,9 @@ import com.hypeapps.instasplit.core.network.api.UserApi
 import com.hypeapps.instasplit.core.network.api.UserExpenseApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 object RetrofitInstance {
-    private const val BASE_URL: String = "http://ngokho.pythonanywhere.com/"
-
     val instaSplitApi: InstaSplitApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -22,5 +22,17 @@ object RetrofitInstance {
 }
 
 interface InstaSplitApi: ExpenseApi, GroupApi, GroupMemberApi, UserApi, UserExpenseApi {
-    // Additional endpoints can be defined here if needed
+    companion object {
+        const val BASE_URL: String = "http://ngokho.pythonanywhere.com/"
+    }
+
+    @GET("api/expenses/image")
+    suspend fun getExpenseImage(): String
+
+    @GET("api/groups/image")
+    suspend fun getGroupImage(): String
+
+    @GET("api/users/image")
+    suspend fun getUserImage(): String
+
 }
