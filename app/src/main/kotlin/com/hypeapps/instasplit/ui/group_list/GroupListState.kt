c86d4655b23895188393c17addc6b5b1bc50c5b4
Nicholas Tietje <tietje.n@northeastern.edu>
2024-04-17 17:12:37 -0400

    add groupWrappers for additional info fetching

--- app/src/main/kotlin/com/hypeapps/instasplit/ui/group_list/GroupListState.kt
+++ app/src/main/kotlin/com/hypeapps/instasplit/ui/group_list/GroupListState.kt
@@ -3,10 +3,12 @@ package com.hypeapps.instasplit.ui.group_list
 import com.hypeapps.instasplit.core.model.entity.Expense
 import com.hypeapps.instasplit.core.model.entity.Group
 import com.hypeapps.instasplit.core.model.entity.User
+import com.hypeapps.instasplit.core.model.entity.bridge.GroupWrapper
 import com.hypeapps.instasplit.core.model.entity.bridge.UserWrapper
 
 data class GroupListState(
     val userWrapper: UserWrapper = UserWrapper.placeholder,
+    val groupWrappers: List<GroupWrapper> = emptyList(),
 ) {
     val user: User
         get() = userWrapper.user
